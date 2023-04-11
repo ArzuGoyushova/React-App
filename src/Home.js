ce4bcd6846acb5ca836d5af3cb7b5cea54ef3de8
ArzuGoyushova <arzusg@code.edu.az>
2023-04-11 22:01:10 +0400

    updated

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,14 @@
 import { useState } from "react";
-
+import BlogList from "./BlogList";
 const Home = () => {
-    //let surname = "Surname1";
-    const [surname, setSurname] = useState("Surname1");
-    const [age, setAge] = useState(26);
-    const changeSurname = () => {
-        setSurname("Surname2");
-        setAge(27);
-    }
-
-    const handleClick = (e) => {
-        console.log("hello", e);
-    }
-    const handleClickAgain = (name,e) => {
-        console.log(name,e.target);
-    }
+  const[blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "manash", id: 1},
+    { title: "Welcome", body: "lorem ipsum...", author: "ezizi", id: 2},
+    { title: "Web dev", body: "lorem ipsum...", author: "muhittin", id: 3}
+  ])
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{surname} is {age} years old.</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e)=>handleClickAgain('arzu',e)}>Click me Again</button>
-            <button onClick={changeSurname}>Change Name</button>
+           <BlogList blogs={blogs} title="My blogs"/>
         </div>
      );
 }
