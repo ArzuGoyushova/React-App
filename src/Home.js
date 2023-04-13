66815cb3709de5fb887a0c7a723bac5febeb649d
ArzuGoyushova <arzusg@code.edu.az>
2023-04-13 18:14:50 +0400

    updated

--- src/Home.js
+++ src/Home.js
@@ -3,29 +3,34 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
   const[blogs, setBlogs] = useState(null)
-  // const [name, setName] = useState("manash");
+  const [isPending, setIsPending] = useState(true);
+  const[error, setError] = useState(null);
 
-  const handleDelete = (id)=>{
-    const newBlogs = blogs.filter((blog)=>blog.id!==id);
-    setBlogs(newBlogs);
-  }
 
   useEffect(()=>{
-    fetch("http://localhost:8000/blogs")
-    .then(res=>{
-      return res.json()
-    })
-    .then(data=>{
-    setBlogs(data);
-    })
+  fetch("http://localhost:8000/blogs")
+  .then(res=>{
+    if(!res.ok){
+      throw Error('couldnt fetch')
+    }
+    return res.json()
+  })
+  .then(data=>{
+  setBlogs(data);
+  setIsPending(false);
+  setError(null);
+  })
+  .catch(err=>{
+    setIsPending(false);
+    setError(err.message)
+  })
   },[])
 
     return ( 
         <div className="home">
-           { blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
-           {/* <BlogList blogs={blogs.filter((blog)=>blog.author==="manash")} title="Manash's blogs" /> */}
-           {/* <p>{name}</p>
-           <button onClick={()=>setName("ezizi")}>change</button> */}
+          { error && <div> {error} </div>}
+          { isPending && <div> Loading... </div>}
+           { blogs && <BlogList blogs={blogs} title="All blogs"/>}
         </div>
      );
 }
