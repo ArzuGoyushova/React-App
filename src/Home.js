4ed443d01869196d31a6676aa12f68b8a76e1624
ArzuGoyushova <arzusg@code.edu.az>
2023-04-13 17:25:36 +0400

    updated

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 import BlogList from "./BlogList";
 const Home = () => {
-  const[blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "manash", id: 1},
-    { title: "Welcome", body: "lorem ipsum...", author: "ezizi", id: 2},
-    { title: "Web dev", body: "lorem ipsum...", author: "muhittin", id: 3}
-  ])
+  const[blogs, setBlogs] = useState(null)
+  // const [name, setName] = useState("manash");
+
+  const handleDelete = (id)=>{
+    const newBlogs = blogs.filter((blog)=>blog.id!==id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(()=>{
+    fetch("http://localhost:8000/blogs")
+    .then(res=>{
+      return res.json()
+    })
+    .then(data=>{
+    setBlogs(data);
+    })
+  },[])
+
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title="My blogs"/>
+           { blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
+           {/* <BlogList blogs={blogs.filter((blog)=>blog.author==="manash")} title="Manash's blogs" /> */}
+           {/* <p>{name}</p>
+           <button onClick={()=>setName("ezizi")}>change</button> */}
         </div>
      );
 }
