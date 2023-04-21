8253f9a930e7ce021403806a625a757ef2b535d3
ArzuGoyushova <arzusg@code.edu.az>
2023-04-21 20:20:36 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Create from './Create';
 
 function App() {
   //burada verilenler teyin etib ashagida {} ichinde chagirmaq olur
   return (
+    <Router>
     <div className="App">
       <Navbar />
      <div className="content">
-      <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/create">
+          <Create/>
+        </Route>
+      </Switch>
      </div>
     </div>
+    </Router>
   );
 }
 
